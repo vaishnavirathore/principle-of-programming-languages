eb69af692e00fa84d3f742f538f12bf75b711d8a
vaishnavirathore <42796756+vaishnavirathore@users.noreply.github.com>
2019-04-04 21:47:02 +0530

    Add files via upload

---
+++ W3-1.cpp
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	char c;
+	printf("Enter 2 numbers:");
+	scanf("%d%d",&a,&b);
+	while(1)
+	{
+		printf("Enter + for addition\nEnter - for subtraction\nEnter * for multiplication\nEnter / for division\nenter % for modulus\nEnter e for exit\n:");
+		scanf("%c",&c);
+		switch(c)
+		{
+			case '+':
+				printf("%d\n",a+b);
+				break;
+			case '-':
+				printf("%d\n",a-b);
+				break;
+			case '*':
+				printf("%d\n",a*b);
+				break;
+			case '/':
+				printf("%d\n",a/b);
+				break;
+			case '%':
+				printf("%d\n",a%b);
+				break;
+			
+			default:
+				printf("Enter right choice\n");
+		}
+	}
+}
